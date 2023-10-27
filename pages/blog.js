1bda1e68a3ff2c75f73552a6eb710153370747c0
Daniel Alejandro Uncos Aciar <da.uncosaciar@gmail.com>
2023-10-26 21:06:45 -0300

    Header finished

---
+++ pages/blog.js
@@ -0,0 +1,9 @@
+import Layout from "../components/layout";
+
+export default function Blog() {
+  return (
+    <Layout title="Blog" description="Blog de música, venta de guitarras, consejos, GuitarLA">
+      <h1>Blog</h1>
+    </Layout>
+  );
+}
--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,11 +1,9 @@
-import Link from "next/link";
 import Layout from "../components/layout";
 
 export default function Nosotros() {
   return (
     <Layout title="Nosotros" description="Sobre Nosotros, GuitarLA, tienda de música">
-      Nosotros.js
-      <Link href="/">Ir a inicio</Link>
+      <h1>Nosotros</h1>
     </Layout>
   );
 }
