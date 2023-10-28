e926cb5c5a534d3d57a80e34c35a0f8f9c65b1d3
Daniel Alejandro Uncos Aciar <da.uncosaciar@gmail.com>
2023-10-28 20:03:23 -0300

    Working on Shop page

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -6,7 +6,7 @@ export default function Nosotros() {
   return (
     <Layout title="Nosotros" description="Sobre Nosotros, GuitarLA, tienda de música">
       <main className="contenedor">
-        <h2 className="heading">Nosotros</h2>
+        <h1 className="heading">Nosotros</h1>
 
         <div className={styles.contenido}>
           <Image src="/img/nosotros.jpg" width={1000} height={800} alt="Imagen sobre nosotros" />
