a37f6ce2a2b62596141660bedc5e836eed2c01a5
Daniel Alejandro Uncos Aciar <da.uncosaciar@gmail.com>
2023-11-08 20:57:23 -0300

    Working on cart layout

--- pages/carrito.js
+++ pages/carrito.js
@@ -1,7 +1,8 @@
+import Image from "next/future/image";
 import Layout from "../components/layout";
 import styles from "../styles/carrito.module.css";
 
-export default function Carrito() {
+export default function Carrito({ carrito, actualizarCantidad }) {
   return (
     <Layout title="Carrito de Compras">
       <main className="contenedor">
@@ -10,6 +11,46 @@ export default function Carrito() {
         <div className={styles.contenido}>
           <div className={styles.carrito}>
             <h2>Artículos</h2>
+
+            {carrito.length === 0
+              ? "Carrito Vacío"
+              : carrito.map((producto) => (
+                  <div key={producto.id} className={styles.producto}>
+                    <div>
+                      <Image width={250} height={480} src={producto.imagen} alt={producto.nombre} />
+                    </div>
+                    <div>
+                      <p className={styles.nombre}>{producto.nombre}</p>
+
+                      <div className={styles.cantidad}>
+                        <p>Cantidad:</p>
+                        <select
+                          className={styles.select}
+                          onChange={(e) =>
+                            actualizarCantidad({
+                              id: producto.id,
+                              cantidad: e.target.value
+                            })
+                          }
+                          value={producto.cantidad}
+                        >
+                          <option value="1">1</option>
+                          <option value="2">2</option>
+                          <option value="3">3</option>
+                          <option value="4">4</option>
+                          <option value="5">5</option>
+                        </select>
+                      </div>
+
+                      <p className={styles.precio}>
+                        $<span>{producto.precio}</span>
+                      </p>
+                      <p className={styles.subtotal}>
+                        Subtotal: $<span>{producto.cantidad * producto.precio}</span>
+                      </p>
+                    </div>
+                  </div>
+                ))}
           </div>
 
           <aside className={styles.resumen}>
